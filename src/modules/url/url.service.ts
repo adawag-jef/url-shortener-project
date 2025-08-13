8d9aff28d34b356a85b4f18e2e022450b466caed
jef-adawag <jeff.adawag@netroadshow.com>
2025-08-13 12:04:14 +0800

    paginate response

--- src/modules/url/url.service.ts
+++ src/modules/url/url.service.ts
@@ -4,6 +4,8 @@ import { DatabaseService } from 'src/database/database.service';
 import { UidService } from '../../services/uid/uid.service';
 import { CreateUrlDto } from './dto/create-url.dto';
 import { UpdateUrlDto } from './dto/update-url.dto';
+import { GetUrlDto } from './dto/get-url.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UrlService {
@@ -30,8 +32,67 @@ export class UrlService {
     return url;
   }
 
-  findAll() {
-    return this.databaseService.url.findMany();
+  async findAll(queryParams: GetUrlDto) {
+    const filter = queryParams.filter;
+    const limit = queryParams.limit || 10;
+    const page = queryParams.page || 1;
+
+    const skip = (page - 1) * limit;
+
+    const whereClause: Prisma.UrlWhereInput = filter
+      ? {
+          OR: [
+            {
+              url: { contains: filter },
+            },
+            {
+              redirect: { contains: filter },
+            },
+            {
+              description: { contains: filter },
+            },
+          ],
+        }
+      : {};
+
+    const data = await this.databaseService.url.findMany({
+      where: whereClause,
+      take: limit,
+      skip,
+    });
+
+    if (data.length === 0) {
+      return [];
+    }
+
+    let baseUrl = `${this.host}/url?limit=${limit}`;
+    if (filter) {
+      baseUrl += `&filter=${encodeURIComponent(filter)}`;
+    }
+
+    const currentPage = page;
+    const perPage = limit;
+    const totalCount = await this.databaseService.url.count({
+      where: whereClause,
+    });
+    const totalPages = Math.ceil(totalCount / limit);
+
+    const nextPage = page < totalPages ? `${baseUrl}&page=${page + 1}` : null;
+    const prevPage = page > 1 ? `${baseUrl}&page=${page - 1}` : null;
+
+    const meta = {
+      totalCount,
+      totalPages,
+      currentPage,
+      perPage,
+      nextPage,
+      prevPage,
+    };
+
+    return {
+      data,
+      meta,
+    };
   }
 
   async findOne(uid: string) {
